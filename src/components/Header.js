d4696fb6bb8246d88d81c2b33bd032db35f06462
Alex Bandisch <alexander.bandisch@gmail.com>
2018-10-21 16:23:09 +1000

    add login/signup to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import { getAuthToken, setAuthToken } from '../lib/utils'
 
 class Header extends React.Component {
   render () {
+    const authToken = getAuthToken()
     return (
       <div className='flex pa1 justify-between nowrap orange'>
         <div className='flex flex-fixed black'>
@@ -10,10 +12,31 @@ class Header extends React.Component {
           <Link to='/' className='ml1 no-underline black'>
             new
           </Link>
-          <div className='ml1'>|</div>
-          <Link to='/create' className='ml1 no-underline black'>
-            submit
-          </Link>
+          {authToken && (
+            <div className='flex'>
+              <div className='ml1'>|</div>
+              <Link to='/create' className='ml1 no-underline black'>
+                submit
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className='flex flex-fixed'>
+          {authToken ? (
+            <div
+              className='ml1 pointer black'
+              onClick={() => {
+                setAuthToken('')
+                this.props.history.push(`/`)
+              }}
+            >
+            logout
+            </div>
+          ) : (
+            <Link to='/login' className='ml1 no-underline black'>
+              login
+            </Link>
+          )}
         </div>
       </div>
     )
