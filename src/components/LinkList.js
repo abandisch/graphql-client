aa0a63a4eda508b37fac374c04caf28cafde6c81
Alex Bandisch <alexander.bandisch@gmail.com>
2018-10-22 08:08:33 +1000

    update store after feed query

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,20 @@ import Link from './Link'
 import { FEED_QUERY } from '../lib/graphql/Queries'
 
 class LinkList extends React.Component {
+  _updateCacheAfterVote (store, createVote, linkId) {
+    // read the current state of the cached data for the FEED_QUERY from the store
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    // retrieve the link that the user just voted for from that list
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+
+    // update the votes array
+    votedLink.votes = createVote.link.votes
+
+    // take the modified data and write it back into the store
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render () {
     return (
       <Query query={FEED_QUERY}>
@@ -13,7 +27,7 @@ class LinkList extends React.Component {
             if (error) return <p>Error! {error}</p>
 
             const linksToRender = data.feed.links
-            return linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} />)
+            return linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} updateStoreAfterVote={this._updateCacheAfterVote} />)
           }
         }
       </Query>
